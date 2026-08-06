2ee5252d35351313dd008be71bfc0ae7f26338ce
Hakan Özkaya <hakanozkayais@outlook.com>
2026-08-06 21:58:38 +0300

    06.08.2026 Dersleri

---
+++ _47_SQL_Ders_SELECT_.sql
@@ -0,0 +1,11 @@
+/* Bu Derste Yeni Bir Tablo Oluşturdum Table_1 Sizde Bulunmayabilir*/
+
+SELECT ID,isim,babaismi FROM Table_1/* Seçili Tabloları Gösteriyor*/
+
+SELECT * FROM Table_1/*  Tüm Tabloları Gösteriupr*/
+select babaismi from Table_1
+
+EXEC sp_databases /*Tablolarımızı Gösteriyor*/
+
+SELECT * FROM SYSOBJECTS WHERE xtype='U'; /* Bütün Tabloları Almak type U Olanları   */
+GO/* Üst Satırdaki Kodları Çalıştırdık*/
---
+++ _48_DISTINCT_TOP.sql
@@ -0,0 +1,6 @@
+SELECT DISTINCT Sehir FROM Musteriler/* DISTINCT Komutu ile aynı olanlar 1 defa getirir birden fazla getirmez*/
+
+SELECT DISTINCT Sehir,MusteriAdi FROM Musteriler
+
+
+SELECT  TOP 10 * FROM Musteriler  /* İlk 10 Kullanıcıları Göstermek*/ 
